7fd9dd4f4f91a8075cc5fc988106d4ba0a3c82f8
hackmagodeoz <hackmagodeoz@gmail.com>
2024-12-30 17:35:00 -0600

    test

--- includes/feeds.sh
+++ includes/feeds.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Add a feed 
-#echo 'src-git mtk_openwrt_feed https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds' >> feeds.conf.default
+echo 'src-git mtk_openwrt_feed https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds' >> feeds.conf.default
